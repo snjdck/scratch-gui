97d53ee4c7952753dbba11a55d2a4e1f74fa33e3
Alex Shao <shao_kai@qq.com>
2017-09-26 16:35:35 +0800

    update

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -36,56 +36,7 @@ class FileHelper {
 class Storage extends ScratchStorage {
     constructor () {
         super();
-        this.fileHelper = new FileHelper();
-    }
-
-    load (assetType, assetId, dataFormat) {
-        /** @type {Helper[]} */
-        const helpers = [this.builtinHelper, this.localHelper, this.fileHelper];
-        const errors = [];
-        let helperIndex = 0;
-        dataFormat = dataFormat || assetType.runtimeFormat;
-
-        return new Promise((fulfill, reject) => {
-            const tryNextHelper = () => {
-                if (helperIndex < helpers.length) {
-                    const helper = helpers[helperIndex++];
-                    helper.load(assetType, assetId, dataFormat)
-                        .then(
-                            asset => {
-                                if (asset === null) {
-                                    tryNextHelper();
-                                } else {
-                                    // TODO? this.localHelper.cache(assetType, assetId, asset);
-                                    if (helper !== this.builtinHelper && assetType.immutable) {
-                                        asset.assetId = this.builtinHelper.cache(
-                                            assetType,
-                                            asset.dataFormat,
-                                            asset.data,
-                                            assetId
-                                        );
-                                    }
-                                    // Note that other attempts may have caused errors, effectively suppressed here.
-                                    fulfill(asset);
-                                }
-                            },
-                            error => {
-                                errors.push(error);
-                                // TODO: maybe some types of error should prevent trying the next helper?
-                                tryNextHelper();
-                            }
-                        );
-                } else if (errors.length === 0) {
-                    // Nothing went wrong but we couldn't find the asset.
-                    fulfill(null);
-                } else {
-                    // At least one thing went wrong and also we couldn't find the asset.
-                    reject(errors);
-                }
-            };
-
-            tryNextHelper();
-        });
+        this.webHelper = new FileHelper();
     }
 }
 
